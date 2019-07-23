ecb629add3b419cca3157537dea739b223be5617
Ryan Wick <rrwick@gmail.com>
2019-07-23 17:37:51 +1000

    Add GPLv3 text to scripts

--- find_unclassified.py
+++ find_unclassified.py
@@ -3,6 +3,17 @@
 This script takes two Centrifuge output files as input. It finds the reads in the first output file
 which are unclassified and then outputs the lines from the second output file which correspond to
 the same reads.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not,
+see <https://www.gnu.org/licenses/>.
 """
 
 import argparse
